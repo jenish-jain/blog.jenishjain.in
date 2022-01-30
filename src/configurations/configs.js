03d2a1a1b95e4798a07c8e5d9881b76ebb1f5961
jenish-jain <jenishjain@rocketmail.com>
2022-01-30 13:33:17 +0530

    added starter blog and reformatted code from starter

--- src/configurations/configs.js
+++ src/configurations/configs.js
@@ -1,3 +1,22 @@
 export const configs = {
+  blogName: "Blog site",
   websiteUrl: 'https://www.jenishjain.in/',
+  twitterHandel: 'jenishjain6',
+  cloudinaryConfigs: {
+    cloudName: 'jenishjain',
+  },
+  sitesInfo: {
+    blogsite: {
+      name: 'blogsite',
+      icon: 'https://avatars.dicebear.com/api/big-ears-neutral/post.svg',
+    },
+    rapidolabs: {
+      name: 'rapido-labs',
+      icon: 'https://res.cloudinary.com/jenishjain/image/upload/v1640938129/portfolio/blog-assets/rapido-labs-icon.png',
+    },
+    medium: {
+      name: 'medium',
+      icon: 'https://res.cloudinary.com/jenishjain/image/upload/v1640938582/portfolio/blog-assets/medium-icon.png',
+    },
+  },
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,17 +13,17 @@ export default function Posts({ posts }) {
       description="A blog site build with toast, mdx and lots of chai."
     />,
     <Layout>
-      <Block color="yellow">
+      <Block color="black">
         <Intro
-          headline={`These are ${posts.length} things i thought would be helpful for others`}
+          headline={`${posts.length} times I’ve sat down to write something as a blog.`}
           post
         >
           <p>
-            This is my personal thought dump.
+            I like to share my learnings, process of building something with others. I am not a regular writer but you can expect something rolling every few months ;)
           </p>
         </Intro>
       </Block>
-      <Block color="white">
+      <Block color="dirty-white">
         <section id="previews">
           <PostPreviews posts={posts} />
         </section>
